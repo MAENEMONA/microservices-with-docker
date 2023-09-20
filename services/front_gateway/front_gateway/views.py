b2e35cf0ec8e9775355dec004bc9356b3ebe8048
MAENEMONA <123475530+MAENEMONA@users.noreply.github.com>
2023-09-20 12:09:32 +0100

    Update views.py
    edited services names

--- services/front_gateway/front_gateway/views.py
+++ services/front_gateway/front_gateway/views.py
@@ -6,19 +6,19 @@ from django.shortcuts import render
 def index(request):
     context = {}
 
-    response = requests.get('http://bangkok:8000/')
+    response = requests.get('http://micro_bangkok:8000/')
     data = response.json()
     context['bangkok'] = data['celsius']
 
-    response = requests.get('http://tokyo:3000/')
+    response = requests.get('http://micro_tokyo:3000/')
     data = response.json()
     context['tokyo'] = data['celsius']
 
-    response = requests.get('http://munich:8000/')
+    response = requests.get('http://micro_munich:8000/')
     data = response.json()
     context['munich'] = data['celsius']
 
-    response = requests.get('http://nyc:9292/')
+    response = requests.get('http://micro_nyc:9292/')
     data = response.json()
     context['nyc'] = data['celsius']
 
